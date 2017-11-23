72c21fe97fa47e794d6565f4ab003fd28eb1c508
Bart van Heukelom <mail@bartvh.nl>
2017-11-23 23:45:16 +0100

    echos

--- backupdisks/welcome_root.sh
+++ backupdisks/welcome_root.sh
@@ -1,20 +1,29 @@
+# This script file is not marked executable because of FAT restrictions.
+# Start by running `bash welcome.sh`
+
 set -e
 
+# --- per disk config --- #
 disk_index=42 #TODO
 # get from blkid
 luks_UUID="QQQQQQQ" #TODO
+
 diskName="BVHBUP${disk_index}"
 partition_boot="${diskName}BOOT"
 partition_data="${diskName}DATA"
 
 
 if [[ ! -f "/mnt/backup/gui.py" ]]; then
+	echo "Opening LUKS volume ${luks_UUID}"
 	cryptsetup open --type luks /dev/disk/by-uuid/${luks_UUID} luks-${partition_data}
+	echo "Mounting in /mnt/backup"
 	mount /dev/mapper/luks-${partition_data} /mnt/backup
 fi
+echo "$partition_data is now mounted at /mnt/backup"
 
 
 pushd /mnt/backup
+echo "Launching gui.py"
 python3 gui.py
 popd
 
