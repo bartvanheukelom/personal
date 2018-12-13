23839d26888dc0f6aa2bca85bb882c49572ea6ce
Bart van Heukelom <mail@bartvh.nl>
2018-12-13 12:13:07 +0100

    add csv writer

--- snippets/csvtransform.py
+++ snippets/csvtransform.py
@@ -4,13 +4,14 @@ import csv
 import sys
 
 inf = sys.stdin
-#inf = open('infile.csv', 'r')
+#inf = open('infile.csv', 'r', encoding="utf-8")
 ouf = sys.stdout
-#ouf = open('outfile.out', 'w')
+#ouf = open('outfile.out', 'w', encoding="utf-8")
 
 r = csv.reader(inf)
+w = csv.writer(ouf)
 
 for row in r:
-	line = 'update table set foo = "' + row[1] + '" where bar = "' + row[0] + '";'
-	ouf.write(line + '\n')
-
+	#line = 'update table set foo = "' + row[1] + '" where bar = "' + row[0] + '";'
+	#ouf.write(line + '\n')
+	w.writerow(row)
